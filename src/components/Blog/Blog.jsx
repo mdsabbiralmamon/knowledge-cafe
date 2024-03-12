5174f369f6d5c63b435e694066d564462e8f79a0
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-03-13 03:18:51 +0600

    dummy blog data added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
+import { CiBookmark } from "react-icons/ci";
 
 const Blog = ({blog}) => {
 
-    const {title, cover, author_image, author, posted_date, reading_time} = blog;
+    const {title, cover, author_image, author, posted_date, reading_time, hashtags} = blog;
 
     const readingTime = reading_time;
     const readingTimeInt = parseInt(readingTime.replace(' mins', ''));
@@ -11,14 +12,24 @@ const Blog = ({blog}) => {
         <div>
             <img src={cover} alt={`cover picture of ${title}`} />
             <div className='flex justify-between'>
-                <div>
-                    <img src={author_image} alt={`image of ${author}`} />
+                <div className='flex gap-4'>
+                    <img className='w-14 rounded-full' src={author_image} alt={`image of ${author}`} />
+                    <div>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
                 </div>
-                <div>
-                    <span>{readingTimeInt} min read</span>
+                <div className='flex gap-3 items-center'>
+                    <div>{readingTimeInt} min read</div>
+                    <button><CiBookmark /></button>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map( (hash, idx)=> <span key ={idx}><a href=''>#{hash} </a></span>)
+                }
+            </p>
         </div>
     );
 };
