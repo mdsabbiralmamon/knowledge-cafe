5858f35ff27464e29b3942239146d159abb1333e
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-03-13 02:54:49 +0600

    dummy blog data fixing

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,24 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-    console.log(blog);
+
+    const {title, cover, author_image, author, posted_date, reading_time} = blog;
+
+    const readingTime = reading_time;
+    const readingTimeInt = parseInt(readingTime.replace(' mins', ''));
+
     return (
         <div>
-            
+            <img src={cover} alt={`cover picture of ${title}`} />
+            <div className='flex justify-between'>
+                <div>
+                    <img src={author_image} alt={`image of ${author}`} />
+                </div>
+                <div>
+                    <span>{readingTimeInt} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
         </div>
     );
 };
