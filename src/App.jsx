82d1c9e44b48cae7fc2ce720958d6a4f0de62231
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-03-12 23:17:15 +0600

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">tailwind connected</h1>
+      <Header></Header>
     </>
   )
 }
